e84ef142809b40307b558333cf21f1661d7062e7
philippgitpush <adeleine1412@gmail.com>
2024-12-05 12:57:19 +0100

    exclude electron from HMR

--- vite.config.js
+++ vite.config.js
@@ -19,4 +19,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    watch: {
+      ignored: ['**/src/electron/*'],
+    },
+  },
 })
