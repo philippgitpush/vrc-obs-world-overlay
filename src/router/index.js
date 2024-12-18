0842d85a014abf2fdded539e33523d103a20cd8b
philippgitpush <adeleine1412@gmail.com>
2024-12-19 00:52:09 +0100

    add dynamic window/route titles

--- src/router/index.js
+++ src/router/index.js
@@ -3,25 +3,41 @@ import DashboardView from '../views/DashboardView.vue';
 import OverlayView from '../views/OverlayView.vue';
 import ConfigView from '../views/ConfigView.vue';
 
+const app_name = 'VRC OBS World Overlay'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'overlay',
-      component: OverlayView
+      component: OverlayView,
+      meta: {
+        title: 'Overlay',
+      }
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: DashboardView
+      component: DashboardView,
+      meta: {
+        title: 'Dashboard',
+      }
     },
     {
       path: '/config',
       name: 'config',
-      component: ConfigView
+      component: ConfigView,
+      meta: {
+        title: 'Settings',
+      }
     },
   ],
+})
+
+router.beforeEach((to) => {
+  const { title } = to.meta;
+  document.title = (app_name ? title + ' – ' + app_name : app_name) || document.title
 })
 
 export default router
